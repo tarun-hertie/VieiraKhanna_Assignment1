db2ec40ff588480af8a84fb44f06f6b4417551f8
tarun khanna <tarun.kh@gmail.com>
2016-10-06 21:48:16 +0200

    Edits to source code

--- data_download.R
+++ data_download.R
@@ -1,5 +1,5 @@
 #running libraries and packages
-install.packages('rio')
+#install.packages('rio')
 library(rio)
 library(plyr)
 library(dplyr)
@@ -9,4 +9,4 @@ alc <- import('https://raw.githubusercontent.com/fivethirtyeight/data/master/alc
 
 #Downloading data set for exercise 2
 mrg <- import('https://raw.githubusercontent.com/fivethirtyeight/data/master/marriage/both_sexes.csv')
-rename(mrg, c("White_2534"="White", "Black_2534"="Black", "Hisp_2534" = "Hispanic"))
+#rename(mrg, c("White_2534"="White", "Black_2534"="Black", "Hisp_2534" = "Hispanic"))

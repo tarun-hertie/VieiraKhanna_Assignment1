20636f3dff918ad36edf4d1749b24b37d10ffbf2
tarun khanna <tarun.kh@gmail.com>
2016-10-07 18:18:14 +0200

    Fianal_updates

--- data_download.R
+++ data_download.R
@@ -9,4 +9,4 @@ alc <- import('https://raw.githubusercontent.com/fivethirtyeight/data/master/alc
 
 #Downloading data set for exercise 2
 mrg <- import('https://raw.githubusercontent.com/fivethirtyeight/data/master/marriage/both_sexes.csv')
-#rename(mrg, c("White_2534"="White", "Black_2534"="Black", "Hisp_2534" = "Hispanic"))
+#mrg <- rename(mrg, c("White_2534"="White", "Black_2534"="Black", "Hisp_2534" = "Hispanic"))
